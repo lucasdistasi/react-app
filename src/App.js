569b5e1047fa756acfb10d25e7c61910167d5e51
Lucas Di Stasi <distasilucas@gmail.com>
2021-04-04 21:23:44 -0300

    Added NavbarComponent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-function App() {
-  return (
-    <h1 className="text-4xl">
-      React App
-    </h1>
-  );
+import React, {Component} from 'react'
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import NavbarComponent from "./component/NavbarComponent";
+
+class App extends Component {
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" exact={true}>
+            <NavbarComponent/>
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
